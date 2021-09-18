335163d413e8f3f7400838d1b82ac243c33814d9
KalpGohil <kalpgohil9@gmail.com>
2021-09-18 08:54:28 +0530

    Done

--- Code/Django/website/accounts/views.py
+++ Code/Django/website/accounts/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User, auth
-from django.contrib import  messages
+from django.contrib import messages
 
 # Create your views here.
 
