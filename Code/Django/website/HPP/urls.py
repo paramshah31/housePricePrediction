8fc1ab3e92beb113ca9ed6e35dfaa87a4b68c8cf
KalpGohil <kalpgohil9@gmail.com>
2021-10-11 11:43:48 +0530

    Done

--- Code/Django/website/HPP/urls.py
+++ Code/Django/website/HPP/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('about', views.about, name='about'),
     path('predict_price', views.predict_house_price, name='predict_price'),
+    path('predict_price_actual', views.predict_price_actual, name='predict_price_actual'),
 ]
--- Code/Django/website/HPP/views.py
+++ Code/Django/website/HPP/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+import pandas as pd
+import pickle
 
 
 # Create your views here.
@@ -11,7 +13,27 @@ def about(request):
 
 
 def predict_house_price(request):
-    return render(request, 'main_input.html')
+    house = pd.read_csv('Bengaluru_House_Data_Cleaned.csv')
+    locations = sorted(house['location'].unique())
+    return render(request, 'main_input.html', {"locations": locations})
+
+
+def predict_price_actual(request):
+    model = pickle.load(open('BangloreHousePricePredictioModel.pkl', 'rb'))
+    location = request.GET['location']
+    sqft = request.GET['sqft']
+    bath = request.GET['bath']
+    bhk = request.GET['bhk']
+
+    print(location, "\n")
+    print(sqft, "\n")
+    print(bath, "\n")
+    print(bhk, "\n")
+
+    prediction = model.predict(pd.DataFrame([[location, bhk, sqft, bath]], columns=["location", "size_BHK", "total_sqft", "bathroom"]))
+    print(prediction, "rupees")
+
+    return render(request, "result.html", {'ans': prediction[0]})
 
 
 
